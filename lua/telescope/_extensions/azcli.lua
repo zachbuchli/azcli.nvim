b06e40f8c4e37509ac508700a4ad3dbf6da10152
Zach Buchli <zachbuchli@pm.me>
2024-12-14 23:10:05 -0800

    renaming telescope extension to be more clear

--- lua/telescope/_extensions/azcli.lua
+++ lua/telescope/_extensions/azcli.lua
@@ -1,5 +1,5 @@
 local azcli = require 'azcli'
 
 return require('telescope').register_extension {
-  exports = { subscriptions = azcli.subscriptions, weblogs = azcli.webapp_logs },
+  exports = { subscriptions = azcli.subscriptions, webapp_logs = azcli.webapp_logs },
 }
